eb8099e7a44e134dbf515bde9afcdf632e212f6d
MattAllman94 <62230366+MattAllman94@users.noreply.github.com>
2020-09-23 18:02:26 +1000

    Began Module 3
    Timer text
    (trouble with scene manager)

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+
+    private float _timeRemaining;
+    private float maxTime = 2 * 60;
+
+    private void Start()
+    {
+        TimeRemaining = maxTime;
+    }
+
+    public void Update()
+    {
+        TimeRemaining -= Time.deltaTime;
+
+        if(TimeRemaining <= 0)
+        {
+            EditorSceneManager.LoadScene(this);
+            TimeRemaining = maxTime;
+        }
+    }
+
+    public float TimeRemaining
+    {
+        get 
+        { 
+            return _timeRemaining; 
+        }
+        set
+        {
+            _timeRemaining = value;
+        }
+    }
+
+
+
+}
---
+++ Assets/Scripts/UpdateUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateUI : MonoBehaviour
+{
+
+    public Text timerLabel;
+       
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timerLabel.text = FormatTime(GameManager.instance.TimeRemaining);
+    }
+
+    private string FormatTime(float timeInSeconds)
+    {
+        return string.Format("(0) : (1:00)", Mathf.FloorToInt(timeInSeconds / 60), Mathf.FloorToInt(timeInSeconds % 60));
+    }
+}
